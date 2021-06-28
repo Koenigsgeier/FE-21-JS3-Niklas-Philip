4b981adf2e73d60a6c56f3910bd8784d2c8238fa
philipm92 <philip.mahlberg@hotmail.com>
2021-06-28 13:56:50 +0200

    did intermediate 3

--- intermediate/js/intermediate3.js
+++ intermediate/js/intermediate3.js
@@ -1,11 +1,17 @@
-// ex 2 | FizzBuzz
+// ex 3 | Starloop
 
 
-document.querySelector(".starloop").innerHTML = "<h1>ex 2 | FizzBuzz</h1><br>";
-
-for (let i = 1; i<=100; i++) {
-    if (i%15 == 0) document.querySelector(".fizzbuz").innerHTML += "<h2>FizzBuzz</h2>";
-    else if (i%3 == 0) document.querySelector(".fizzbuz").innerHTML += "<strong>Fizz</strong> <br>";
-    else if (i%5 == 0) document.querySelector(".fizzbuz").innerHTML += "<em>Buzz</em> <br>";
-    else document.querySelector(".fizzbuz").innerHTML += i + " <br>";
+document.querySelector(".starloop").innerHTML = "<h1>ex 3 | Starloop</h1>";
+var char = "*";
+const colors = ["red", "green", "blue"];
+const fweight = ["normal", "bold", "900"];
+for (let i = 1; i<=10; i++) {
+    // document.querySelector(".starloop").innerHTML += "*" * i + "<br>";
+    // var j = 1;
+    var mul_str = "";
+    for (let j = 1; j<=i; j++)  {
+        mul_str += `<strong style="color:${colors[(j-1)%3]};font-weight:${fweight[(j-1)%3]};">${char}</strong>`; 
+    }
+    document.querySelector(".starloop").innerHTML += `<p>${mul_str}</p>`;
 }
+
