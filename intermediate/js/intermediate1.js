16914f121e003f82cbbc0faf6c10e153f70a35d8
philipm92 <philip.mahlberg@hotmail.com>
2021-06-28 14:03:05 +0200

    added names to intermediate 1

--- intermediate/js/intermediate1.js
+++ intermediate/js/intermediate1.js
@@ -3,8 +3,9 @@
 document.getElementsByTagName("p")[0].innerHTML = "The student grades are:<br>";
 
 var student_points = [76,85,65,93,81];
-var sum_points = 0;
+var student_names = ["Martin", "Thomas" ," Klaus", "Maria", "David"];
 
+var sum_points = 0;
 function GetGrade(points) {
     if (points < 60) return "F";
     else if (points < 70) return "D";
@@ -13,19 +14,22 @@ function GetGrade(points) {
     else return "A";
 }
 
+var i = 0;
 for (let points of student_points) {
     let grade = GetGrade(points);
+    let name = student_names[i];
     switch (grade) {
         case "F": 
         case "A":
-            document.getElementsByTagName("p")[0].innerHTML += `With ${points} points you got an <b>${grade}</b><br>`;
+            document.getElementsByTagName("p")[0].innerHTML += `With ${points} points ${name} got an <b>${grade}</b>.<br>`;
             break;
         default: 
-            document.getElementsByTagName("p")[0].innerHTML += `With ${points} points you got a <b>${grade}</b><br>`;
+            document.getElementsByTagName("p")[0].innerHTML += `With ${points} points ${name} got a <b>${grade}</b>.<br>`;
             break;
     }
     // sum
     sum_points += points;
+    i++;
     
 }
 
@@ -34,10 +38,10 @@ var average_grade = GetGrade(sum_points / student_points.length);
 switch (average_grade) {
     case "F": 
     case "A":
-        document.getElementsByTagName("p")[0].innerHTML += `<br>With ${sum_points / student_points.length} points the average grade is an <strong>${average_grade}</strong>.`;
+        document.getElementsByTagName("p")[0].innerHTML += `<br>With ${sum_points / student_points.length} points the average class grade is an <strong>${average_grade}</strong>.`;
         break;
     default:
-        document.getElementsByTagName("p")[0].innerHTML += `<br>With ${sum_points / student_points.length} points the average grade is a <strong>${average_grade}</strong>.`;
+        document.getElementsByTagName("p")[0].innerHTML += `<br>With ${sum_points / student_points.length} points the average class grade is a <strong>${average_grade}</strong>.`;
         break;
 
 }
