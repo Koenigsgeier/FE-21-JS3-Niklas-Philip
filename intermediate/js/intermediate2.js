9e5d772cc98deb95c801bb74edb6245c09f50117
philipm92 <philip.mahlberg@hotmail.com>
2021-06-28 13:17:06 +0200

    did some styling for better looks

--- intermediate/js/intermediate2.js
+++ intermediate/js/intermediate2.js
@@ -4,8 +4,8 @@
 document.querySelector(".fizzbuz").innerHTML = "<h1>ex 2 | FizzBuzz</h1><br>";
 
 for (let i = 1; i<=100; i++) {
-    if (i%15 == 0) document.querySelector(".fizzbuz").innerHTML += "FizzBuzz <br>";
-    else if (i%3 == 0) document.querySelector(".fizzbuz").innerHTML += "Fizz <br>";
-    else if (i%5 == 0) document.querySelector(".fizzbuz").innerHTML += "Buzz <br>";
+    if (i%15 == 0) document.querySelector(".fizzbuz").innerHTML += "<h2>FizzBuzz</h2>";
+    else if (i%3 == 0) document.querySelector(".fizzbuz").innerHTML += "<strong>Fizz</strong> <br>";
+    else if (i%5 == 0) document.querySelector(".fizzbuz").innerHTML += "<em>Buzz</em> <br>";
     else document.querySelector(".fizzbuz").innerHTML += i + " <br>";
 }
